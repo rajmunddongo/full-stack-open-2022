9ceaebe82c44691d70d67c39b69dd5fd980652b2
unknown <radzsmir@gmail.com>
2022-10-07 23:44:11 +0200

    1.6

---
+++ part1/1.6/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.setToGood}>
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => {
+    setGood(good+1)
+  }
+  const setToNeutral = newValue => {
+    setNeutral(neutral+1)
+  }
+  const setToBad = newValue => {
+    setBad(bad+1)
+  }
+
+  return (
+    
+    <div>
+      <h1>
+        give feedback
+      </h1>
+      <button onClick={setToGood}>
+        good
+      </button>
+      <button onClick={setToNeutral}>
+        neutral
+      </button>
+      <button onClick={setToBad}>
+        bad
+      </button>
+      <h1>
+        statistics
+      </h1>
+      <p>
+        good {good}
+      </p>
+      <p>
+        neutral {neutral}
+      </p>
+      <p>
+        bad {bad}
+      </p>
+    </div>
+  )
+}
+
+export default App

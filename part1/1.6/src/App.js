49e94dd43be6451ac237f5ca80f15f3824617ade
rajmunddongo <rajmund.dongo@gmail.com>
2022-10-10 22:13:09 +0200

    1.12

--- part1/1.6/src/App.js
+++ part1/1.6/src/App.js
@@ -1,97 +1,26 @@
 import { useState } from 'react'
-
-const StatisticLine = (props) =>{
-  if(props.text=="positive"){
-    return(
-      <tr>
-        <td>
-        {props.text}
-        </td>
-        <td>
-        {props.value}%
-        </td>
-      </tr>
-    )
-  }
-  return(
-    <tr>
-      <td>
-      {props.text}
-      </td>
-      <td>
-      {props.value}
-      </td>
-    </tr>
-  )
-}
-const Button = (props) =>{
-
-  return(
-    
-      <button onClick={props.func}>{props.text}</button>
-    
-  )
-}
-
-const Statistics = (props) => {
-  const all = props.bad + props.neutral + props.good
-  return (
-    <div>
-      <table>
-      <tbody>
-      <StatisticLine text="good" value ={props.good}/>
-      <StatisticLine text="neutral" value ={props.neutral}/>
-      <StatisticLine text="bad" value ={props.bad}/>
-      <StatisticLine text="all" value ={all}/>
-      <StatisticLine text="average" value ={(props.bad * -1 + props.good) / (props.bad + props.neutral + props.good)}/>
-      <StatisticLine text="positive" value ={props.good / (props.bad + props.neutral + props.good)}/>
-      </tbody>
-      </table>
-    </div>
-  )
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const setToGood = newValue => {
-    setGood(good + 1)
-  }
-  const setToNeutral = newValue => {
-    setNeutral(neutral + 1)
-  }
-  const setToBad = newValue => {
-    setBad(bad + 1)
-  }
-  if (good + bad + neutral == 0) {
-    return (
-      <div>
-        <h1>
-          give feedback
-        </h1>
-      <Button func={setToGood} text='good'></Button>
-      <Button func={setToNeutral} text='neutral'></Button>
-      <Button func={setToBad} text='bad'></Button>
-        <p>No feedback given.</p>
-      </div>
-    )
-  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  console.log(anecdotes.length)
+  console.log(selected)
   return (
-
     <div>
-      <h1>
-        give feedback
-      </h1>
-      <Button func={setToGood} text='good'></Button>
-      <Button func={setToNeutral} text='neutral'></Button>
-      <Button func={setToBad} text='bad'></Button>
-      <h1>
-        statistics
-      </h1>
-      <Statistics bad={bad} neutral={neutral} good={good}></Statistics>
+      {anecdotes[selected]}
+      <p>
+        <button onClick={()=>setSelected( Math.round(Math.random() * (anecdotes.length-1) ))}>
+          next anecdote
+        </button>
+      </p>
     </div>
   )
 }
